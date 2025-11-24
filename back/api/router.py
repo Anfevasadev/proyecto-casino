a60e71351bb09c3c6694ecaed522ba9c0121ab00
JoseDavidDev <josecano1128059@correo.itm.edu.co>
2025-11-23 19:08:44 -0500

    Update router.py

--- back/api/router.py
+++ back/api/router.py
@@ -29,8 +29,9 @@ from back.api.v1.auth import router as auth_router
 from back.api.v1.users import router as users_router
 from back.api.v1.places import router as places_router
 
+# Prefijo correcto para que Swagger lo muestre como /api/v1/...
+api_router = APIRouter(prefix="/api/v1")
 
-api_router = APIRouter(prefix="/v1")
 api_router.include_router(auth_router, tags=["auth"]) 
 api_router.include_router(users_router, prefix="/users", tags=["users"])
 api_router.include_router(places_router, prefix="/places", tags=["places"])
